132f7ab8b12e4ce78a5ded85fee6b9275587ebcd
Victor Velchev <victor.velchev@gmail.com>
2018-06-20 03:46:49 -0400

    Wallet refactured

--- wallet.py
+++ wallet.py
@@ -1,5 +1,5 @@
 import requests, datetime, hashlib, json, os, binascii
-
+from pycoin.ecdsa import generator_secp256k1
 
 seed = None
 chain_number = None
@@ -17,26 +17,50 @@ class Wallet(object):
     def defined(self, obj: object) -> bool:
         if obj == None or obj == "": return False
         return True
-
-    def get_bytes_random_string(self, length:int) -> bytes:
-        if self.defined(length) == False: return False
-        if isinstance(length, int) == False: return False
-        return os.urandom(length)
-    
-    def convert_bytes_string_to_int(self, hex_str:bytes, length:int) -> int:
-        if self.defined(hex_str) == False: return False
-        if isinstance(hex_str, bytes) == False: return False
-        if self.defined(length) == False: return False
-        if isinstance(length, int) == False: return False
-        return int(binascii.hexlify(hex_str), 16)
     
     def get_next_private_key(self):
         global seed, chain_number
         chain_number_string = str(chain_number).encode("utf8")
         chain_number += 1
-        seed_string = seed.decode()
-        return hashlib.sha256(chain_number_string + seed_string).hexdigest()
+        seed_string = seed
+        return hashlib.sha256(chain_number_string + seed_string.encode("utf8")).hexdigest()
+    
+    def get_compressed_public_key_from_private_key(self, private_key:int) -> int:
+        private_key_int = self.convert_string_to_int(private_key, 16)
+        public_key_int = self.convert_private_key_to_public_key_int(private_key_int)
+        compressed_public_key = self.get_compressed_public_key_from_int(public_key_int)
+        print("Your compressed public key:", compressed_public_key)
+        return compressed_public_key
+    
+    def convert_string_to_int(self, string:str, length:int) -> int:
+        if self.defined(string) == False: return False
+        if isinstance(string, str) == False: return False
+        if self.defined(length) == False: return False
+        if isinstance(length, int) == False: return False
+        return int(string, 16)
+    
+    def convert_private_key_to_public_key_int(self, private_key:int) -> int:
+        if self.defined(private_key) == False: return False
+        if isinstance(private_key, int) == False: return False
+        if int(private_key) < 0: return False
+        return (generator_secp256k1 * private_key).pair()
+    
+    def get_compressed_public_key_from_int(self, public_key:int) -> str:
+        if self.defined(public_key) == False: return False
+        if isinstance(public_key, tuple) == False: return False
+        return (hex(public_key[0]))[2:] + str(public_key[1] % 2)
+    
+    def get_address_from_compressed_public_key(self, compressed_public_key:str)-> str:
+        if self.defined(compressed_public_key) == False: return False
+        if isinstance(compressed_public_key, str) == False: return False
+        param = public_key.encode("utf8")
+        hashed_param = hashlib.new("ripemd160", param).hexdigest()
+        print("Your address:", hashed_param)
+        return hashed_param
         
     
 wlt = Wallet()
-new_private_key = wlt.get_next_private_key
+private_key = wlt.get_next_private_key()
+print("Your private key is", private_key)
+public_key = wlt.get_compressed_public_key_from_private_key(private_key)
+address = wlt.get_address_from_compressed_public_key(public_key)
